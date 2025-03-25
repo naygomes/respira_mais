e89b6f1700c8fdd426656d2492feb1d0c60e66db
naygomes <nayara.gomes13@poli.ufrj.br>
2025-03-25 03:41:59 -0300

    feat - creating a polygon area for test

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MapContainer, Marker, TileLayer, Popup } from 'react-leaflet';
+import { MapContainer, Marker, TileLayer, Polygon, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
@@ -8,6 +8,15 @@ import { MapProps } from '@/interfaces';
 import { HOST_MAP } from '../../../settings';
 
 const Map = ({ position, location, zoom = 12 }: MapProps) => {
+  const bairroCoordinates: [number, number][] = [
+    [-23.034538, -43.492552],
+    [-23.012702, -43.390679],
+    [-22.994675, -43.390867],
+    [-22.995196, -43.432859],
+    [-23.020379, -43.508373],
+    [-23.03698, -43.49891],
+  ];
+
   return (
     <MapContainer
       center={position}
@@ -22,6 +31,9 @@ const Map = ({ position, location, zoom = 12 }: MapProps) => {
       <Marker position={location}>
         <Popup></Popup>
       </Marker>
+      <Polygon positions={bairroCoordinates} color="blue">
+        <Popup>Informações sobre o bairro.</Popup>
+      </Polygon>
     </MapContainer>
   );
 };
