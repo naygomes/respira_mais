72295b91207fbed9306109e60eeb75ded0097b3e
naygomes <nayara.gomes13@poli.ufrj.br>
2025-03-25 00:57:43 -0300

    feat - putting header fixed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
 import { Button } from '@/components';
-import { logo } from '@/assets';
+import { logotipo } from '@/assets';
 
 export default function Header() {
   return (
-    <div className="bg-light flex flex-row items-center justify-between px-16 box-content py-4 gap-[4vw]">
-      <Image className="w-40" src={logo} alt="logo do site" />
+    <div className="fixed w-screen z-1 bg-light flex flex-row items-center justify-between px-16 box-border py-4 gap-[4vw]">
+      <Image className="w-40" src={logotipo} alt="logo do site" />
       <div className="flex flex-row items-center gap-[8vw]">
         <Button type="text">Mapa Interativo</Button>
         <Button type="text">Mais recentes</Button>
