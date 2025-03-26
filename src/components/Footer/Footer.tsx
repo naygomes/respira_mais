3585462fea156fcf98be0798c3ba5edfe88f1958
naygomes <nayara.gomes13@poli.ufrj.br>
2025-03-26 01:42:15 -0300

    refactor - changing footer and header background color

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="w-screen bg-light flex flex-row items-center justify-between px-16 box-border py-10">
+    <div className="w-screen bg-background flex flex-row items-center justify-between px-16 box-border py-10">
       <p className="text-xl">
         Desenvolvido por <b>Nayara Gomes</b>
       </p>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import { logotipo } from '@/assets';
 
 export default function Header() {
   return (
-    <div className="fixed w-screen z-1 bg-light flex flex-row items-center justify-between px-16 box-border py-4 gap-[4vw]">
+    <div className="fixed w-screen z-1 bg-background shadow-xl flex flex-row items-center justify-between px-16 box-border py-4 gap-[4vw]">
       <Image className="w-40" src={logotipo} alt="logo do site" />
       <div className="flex flex-row items-center gap-[8vw]">
         <Button type="text">Mapa Interativo</Button>
