c204ef9ef668afeda3fff58b764831df5bd43e7b
naygomes <nayara.gomes13@poli.ufrj.br>
2025-03-25 23:50:04 -0300

    refactor - map estructure

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -6,6 +6,7 @@ import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 import { MapProps } from '@/interfaces';
 import { HOST_MAP } from '../../../settings';
+import './Map.css';
 
 const Map = ({ position, location, zoom = 12 }: MapProps) => {
   const bairroCoordinates: [number, number][] = [
@@ -18,23 +19,25 @@ const Map = ({ position, location, zoom = 12 }: MapProps) => {
   ];
 
   return (
-    <MapContainer
-      center={position}
-      zoom={zoom}
-      scrollWheelZoom={false}
-      style={{ width: '100%', height: '100%' }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url={HOST_MAP}
-      />
-      <Marker position={location}>
-        <Popup></Popup>
-      </Marker>
-      <Polygon positions={bairroCoordinates} color="blue">
-        <Popup>Informações sobre o bairro.</Popup>
-      </Polygon>
-    </MapContainer>
+    <div id="page-map" className="pt-23">
+      <MapContainer
+        center={position}
+        zoom={zoom}
+        scrollWheelZoom={false}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={HOST_MAP}
+        />
+        <Marker position={location}>
+          <Popup></Popup>
+        </Marker>
+        <Polygon positions={bairroCoordinates} color="blue">
+          <Popup>Informações sobre o bairro.</Popup>
+        </Polygon>
+      </MapContainer>
+    </div>
   );
 };
 
