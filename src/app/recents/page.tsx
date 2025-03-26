ddbb069296e18af9bc8e93c5053821b6a27a9465
naygomes <nayara.gomes13@poli.ufrj.br>
2025-03-26 04:55:36 -0300

    feat - creating recents page

--- src/app/recents/page.tsx
+++ src/app/recents/page.tsx
@@ -1,8 +1,32 @@
-export default function Recents() {
+'use client';
+
+import { Table, Text, Title } from '@/components';
+import { useNeighborhoodData } from '@/hooks';
+import { tableHeadList } from '@/utils';
+
+const cta = 'Vai sair? Chega mais e aproveite o dia numa boa.';
+const description =
+  'No Respira+, você fica por dentro de como tá o ar em qualquer canto da cidade para aproveitar o Rio com a tranquilidade que só o carioca sabe ter.';
+
+const Recents = () => {
+  const { neighborhoodData, loading, error } = useNeighborhoodData();
+
+  if (loading)
+    return (
+      <p className="text-secondary text-xl animate-pulse h-screen flex items-center justify-center">
+        Carregando...
+      </p>
+    );
+  if (error) return <p>Erro: {error}</p>;
+
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="flex flex-col items-center justify-center w-screen">
+      <div className="text-center mt-12 mx-32">
+        <Title>{cta}</Title>
+        <Text>{description}</Text>
+      </div>
+      <Table headList={tableHeadList} data={neighborhoodData} />
     </div>
   );
-}
-1;
+};
+export default Recents;
